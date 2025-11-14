e0453a4ddf15c9fc743d6920f9022cc66fc446b4
Josh <15219491+jkaps9@users.noreply.github.com>
2025-11-14 00:26:15 -0500

    update active class of buttons on click

--- src/index.js
+++ src/index.js
@@ -5,6 +5,32 @@ import "./local.css";
 const header = document.querySelector("header");
 const hamburger = document.querySelector("button.hamburger");
 
-hamburger.addEventListener("click", () => {
-  header.classList.toggle("active");
-});
+if (hamburger) {
+  hamburger.addEventListener("click", () => {
+    if (header) {
+      header.classList.toggle("active");
+    }
+  });
+}
+
+const slideIndicators = document.querySelectorAll("button.dot");
+var currentId = 1;
+
+if (slideIndicators) {
+  slideIndicators.forEach((indicator) => {
+    indicator.addEventListener("click", () => {
+      const newId = Number(indicator.getAttribute("data-id"));
+      if (newId !== currentId) {
+        currentId = newId;
+        removeActiveClass(slideIndicators);
+        indicator.className = "dot active";
+      }
+    });
+  });
+}
+
+function removeActiveClass(elements) {
+  elements.forEach((element) => {
+    element.classList.remove("active");
+  });
+}
